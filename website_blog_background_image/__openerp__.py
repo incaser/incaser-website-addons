79bd1325b8a25d789cb056445a1df0f5419a4532
Sergio Incaser <sergio@incaser.es>
2016-01-20 17:38:53 +0100

    [NEW] website_blog_background_image

---
+++ website_blog_background_image/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Website Blog Background Image',
+    'summary': 'Select an image to assign it to blg post',
+    'version': '8.0.1.0.0',
+    'category': "website",
+    'author': 'Incaser Informatica S.L., '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': ['website_blog'],
+    'data': [
+        'views/website_blog_view.xml',
+    ],
+    'image': [],
+    'installable': True,
+}
---
+++ website_blog_background_image/models/website_blog.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import fields, models
+
+
+class BlogPost(models.Model):
+    _inherit = 'blog.post'
+
+    background_image = fields.Binary(string='Background Image')

d5907800e8cb033ede96d5cde347f36d41f0bd29
Sergio Incaser <sergio@incaser.es>
2016-02-22 11:45:00 +0100

    [NEW[8.0] website_slides_no_comment: Not allow slide comments

---
+++ website_slides_no_comment/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Incaser Informatica S.L. - Sergio Teruel
+# (c) 2015 Incaser Informatica S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Website Slides No Comment',
+    'summary': 'Not allow write slides comments',
+    'version': '8.0.1.0.0',
+    'category': 'website',
+    'author': 'Incaser Informatica S.L., '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_slides',
+    ],
+    'data': [
+        'views/website_slides.xml',
+    ],
+    'images': [],
+    'installable': True,
+}

bab390a599bd5ebf85164146a8bceb53e3fe13ea
sergio-incaser <sergio@incaser.es>
2016-04-13 21:34:30 +0200

    [IMP][8.0] website_slides_notice: Select notice level message

--- website_slides_notice/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import main
--- website_slides_notice/models/slides.py
+++ website_slides_notice/models/slides.py
@@ -2,10 +2,16 @@
 # © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models, SUPERUSER_ID, _
+from openerp import fields, models
 
 
 class SlideCategory(models.Model):
     _inherit = 'slide.category'
 
+    notice_level = fields.Selection([
+        ('alert-success', 'Success'),
+        ('alert-info', 'Info'),
+        ('alert-warning', 'Warning'),
+        ('alert-danger', 'Danger'),
+    ], default='alert-info', string='Level Notice')
     notice = fields.Html(string='Notice', translate=True)
